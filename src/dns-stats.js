c53a0f6f0fbfbad1e3c2e5cdd6de0c739f98df80
Pashkova Daria <dar.pashcovaa@gmail.com>
2023-12-19 14:00:06 +0900

    feat: solve dns-stats task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const dnsStats = {}; 
+
+  for (let i = 0; i < domains.length; i+=1) {
+    const parts = domains[i].split('.').reverse(); 
+    let domain = ''; 
+
+    for (let j = 0; j < parts.length; j+=1) {
+      domain += `.${parts[j]}`; 
+      dnsStats[domain] = (dnsStats[domain] || 0) + 1; 
+    }
+  }
+
+  return dnsStats; 
 }
 
 module.exports = {
