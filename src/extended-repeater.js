9a19becdbaf9f5029f34a7309c4968fdb2735908
Pashkova Daria <dar.pashcovaa@gmail.com>
2023-12-19 14:35:15 +0900

    feat: solve extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(inputString, options) {
+  let result = '';
+  let additionResult = [];
+
+  // Установка значений по умолчанию для параметров, если они не были предоставлены
+  let {
+    repeatTimes = 1,
+    separator = '+',
+    addition = '',
+    additionRepeatTimes = 1,
+    additionSeparator = '|'
+  } = options;
+
+  // Формирование строки 'addition' с учетом повторений и разделителя
+  for (let j = 0; j < additionRepeatTimes; j++) {
+    additionResult.push(String(addition));
+  }
+  let additionString = additionResult.join(additionSeparator);
+
+  // Формирование конечной строки с учетом повторений и разделителя
+  for (let i = 0; i < repeatTimes; i++) {
+    result += String(inputString) + additionString;
+
+    // Добавление разделителя, если это не последняя итерация
+    if (i !== repeatTimes - 1) {
+      result += separator;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
